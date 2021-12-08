56df6db6441e63b0966d85a042c4ba86c7ff86b3
PowerNether <slavacraft07@gmail.com>
2021-12-08 21:38:10 +0500

    fix

--- js/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function () {
-    if (window.innerWidth < 768) {
-        new fullpage('#fullpage', {
-            sectionsColor : ['#fff', '#fff', '#fff', '#fff'],
-        });
-    }
-});
